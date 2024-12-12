3863bd9c61be1314b94d7445c484766cc91fc450
十五 <1403368862@qq.com>
2024-12-12 15:25:34 +0800

    feat(aigou-api):增加系统启动后的日志输出 - 在 Application 类中添加日志记录器 - 在 main 方法中增加启动成功后的日志输出，包括服务器端口和上下文路径

--- aigou-api/src/main/java/com/training/aigouapi/Application.java
+++ aigou-api/src/main/java/com/training/aigouapi/Application.java
@@ -1,16 +1,29 @@
 package com.training.aigouapi;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * @author ethan
  */
 @SpringBootApplication
 public class Application {
+    private static final Logger logger = Logger.getLogger(Application.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+        // 启动Spring Boot应用
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        // 获取Environment实例
+        Environment env = context.getEnvironment();
 
+        // 获取端口号和上下文路径
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path", "");
+
+        // 输出启动信息
+        logger.info("\n\n===========> 系统启动成功！后台地址：http://localhost:" + serverPort + contextPath);
+    }
 }
