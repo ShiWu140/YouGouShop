1a95f20546b8b695707e3d91101460a7967cb4ea
ethanzhao2001 <ethanzhao2001@aliyun.com>
2024-12-17 16:11:22 +0800

    feat(aigou-api): 添加全局异常处理

---
+++ aigou-api/src/main/java/com/training/aigouapi/advice/ProjectExceptionAdvice.java
@@ -0,0 +1,24 @@
+package com.training.aigouapi.advice;
+
+import com.training.aigouapi.common.Result;
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理
+ *
+ * @author ethan
+ * @version 1.0
+ * @since 2024/12/13 02:49
+ */
+@RestControllerAdvice
+public class ProjectExceptionAdvice {
+    private static final Logger logger = Logger.getLogger(ProjectExceptionAdvice.class);
+
+    @ExceptionHandler
+    public Result handleException(Exception e) {
+        logger.error(e.getMessage());
+        return Result.error("服务器开小差了，请稍后重试！");
+    }
+}
