f9db27c5799182509e626ec5caecc714cc8b7346
like <15165136+likezhian@user.noreply.gitee.com>
2024-12-18 18:29:19 +0800

    新增 ReceivingAddressMapper.xml、ShopCartMapper.xml 和 ShopCartProductMapper.xml 映射文件

--- aigou-api/src/test/java/com/training/aigouapi/ApplicationTests.java
+++ aigou-api/src/test/java/com/training/aigouapi/ApplicationTests.java
@@ -1,11 +1,12 @@
 package com.training.aigouapi;
 
 import com.training.aigouapi.mapper.BrandMapper;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.annotation.Resource;
+
 
 @SpringBootTest
 class ApplicationTests {
