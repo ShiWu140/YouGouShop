b4e72d58329567b643bb8af47a4d5f71420b96e1
ethanzhao2001 <ethanzhao2001@aliyun.com>
2024-12-19 09:14:26 +0800

    config(aigou-shop-api): 添加全局 CORS 配置

---
+++ aigou-shop-api/src/main/java/com/training/aigoushopapi/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.training.aigoushopapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS
+ *
+ * @author ethan
+ * @version 1.0
+ * @since 2024/12/19 09:13
+ * 新增 WebConfig 类实现 WebMvcConfigurer 接口
+ * 配置跨域请求（CORS）设置，允许所有来源的请求
+ * 支持 GET、POST、PUT、DELETE 和 OPTIONS 方法
+ * 允许所有头部信息和凭证- 设置预检请求的缓存时间为 3600 秒
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
